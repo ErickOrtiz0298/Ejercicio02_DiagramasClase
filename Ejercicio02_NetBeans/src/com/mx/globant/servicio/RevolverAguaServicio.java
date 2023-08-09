24e64ac25bdc0b7dfdc75a407789caca7a798f85
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-08-09 17:00:27 -0600

    Se pusieron publicos los metodos y se mejoro la logica del metodo mojar

--- Ejercicio02_NetBeans/src/com/mx/globant/servicio/RevolverAguaServicio.java
+++ Ejercicio02_NetBeans/src/com/mx/globant/servicio/RevolverAguaServicio.java
@@ -5,12 +5,12 @@ import java.lang.Math;
 
 public class RevolverAguaServicio {
     
-    protected static RevolverAgua crearRevolver(){
+    public static RevolverAgua crearRevolver(){
         RevolverAgua r = new RevolverAgua();
         return r;
     }
     
-    protected static void llenarRevolver(RevolverAgua revolver){
+    public static void llenarRevolver(RevolverAgua revolver){
         int rand = (int)(Math.random() * 6 - 1 ) + 1;
         revolver.setPosicionAgua(rand);
         revolver.setPosicionActual(rand);
@@ -18,10 +18,11 @@ public class RevolverAguaServicio {
     }
     
     protected static boolean mojar(RevolverAgua revolver){
-        if(revolver.getPosicionActual() == revolver.getPosicionAgua()){
+       /* if(revolver.getPosicionActual() == revolver.getPosicionAgua()){
             return true;
         }
-        return false; 
+        return false; */
+       return(revolver.getPosicionActual() == revolver.getPosicionAgua());
     }
     
     protected static void siguienteChorro(RevolverAgua revolver){
@@ -29,7 +30,7 @@ public class RevolverAguaServicio {
             revolver.setPosicionActual(1);
         }else{
             revolver.setPosicionActual(revolver.getPosicionActual() +1 );
-        };
+        }
     }
     
     protected static String toString(RevolverAgua revolver){
